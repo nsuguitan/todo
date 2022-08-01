ee36e0888c22b79eac899084f105b952cae0a990
Nadia Suguitan <nadia.suguitan@gmail.com>
2022-08-01 11:32:03 -0400

    add update request method

--- src/main/kotlin/com/kotlindemo/todo/controllers/ToDoController.kt
+++ src/main/kotlin/com/kotlindemo/todo/controllers/ToDoController.kt
@@ -2,12 +2,8 @@ package com.kotlindemo.todo.controllers
 
 import com.kotlindemo.todo.entities.Todo
 import com.kotlindemo.todo.entities.TodoRepository
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.web.bind.annotation.*
 import java.util.Optional
 
 
@@ -26,5 +22,15 @@ class ToDoController(val todoRepository: TodoRepository) {
     @PostMapping
     fun newTodo(@RequestBody todo: Todo): Todo{
         return todoRepository.save(todo)
+    }
+
+    @PutMapping("/{todoId}")
+    fun updateTodo(@PathVariable("todoId") todoId: Long, @RequestBody updatedTodo: Todo): Todo? {
+        val oldTodo = todoRepository.findByIdOrNull(todoId)
+        if(oldTodo == null){
+            return oldTodo
+        }
+
+        return todoRepository.save(updatedTodo)
     }
 }
