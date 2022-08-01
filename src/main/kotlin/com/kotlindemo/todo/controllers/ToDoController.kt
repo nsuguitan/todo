8969fc70c08913b21d95e8207cabf651f1d3423b
Nadia Suguitan <nadia.suguitan@gmail.com>
2022-08-01 10:41:52 -0400

    Get Requests methods added

--- src/main/kotlin/com/kotlindemo/todo/controllers/ToDoController.kt
+++ src/main/kotlin/com/kotlindemo/todo/controllers/ToDoController.kt
@@ -2,18 +2,29 @@ package com.kotlindemo.todo.controllers
 
 import com.kotlindemo.todo.entities.Todo
 import com.kotlindemo.todo.entities.TodoRepository
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.Optional
 
 
 @RestController
 @RequestMapping("/todos")
 class ToDoController(val todoRepository: TodoRepository) {
 
+    @GetMapping
+    fun getTodos() = todoRepository.findAll()
+
+    @GetMapping("/{todoId}")
+    fun getTodo(@PathVariable("todoId") todoId: Long): Optional<Todo> {
+        return todoRepository.findById(todoId)
+    }
+
     @PostMapping
-    fun newToDo(@RequestBody todo: Todo): Todo{
+    fun newTodo(@RequestBody todo: Todo): Todo{
         return todoRepository.save(todo)
     }
 }
