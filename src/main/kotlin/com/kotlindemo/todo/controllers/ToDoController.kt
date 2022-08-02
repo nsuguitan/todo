7e889b49341397e24e1b774a626be2fbe56835a5
Nadia Suguitan <nadia.suguitan@gmail.com>
2022-08-02 11:32:55 -0400

    cors issue fix

--- src/main/kotlin/com/kotlindemo/todo/controllers/ToDoController.kt
+++ src/main/kotlin/com/kotlindemo/todo/controllers/ToDoController.kt
@@ -2,6 +2,7 @@ package com.kotlindemo.todo.controllers
 
 import com.kotlindemo.todo.entities.Todo
 import com.kotlindemo.todo.entities.TodoRepository
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.web.bind.annotation.*
 import java.util.Optional
@@ -11,19 +12,23 @@ import java.util.Optional
 @RequestMapping("/todos")
 class ToDoController(val todoRepository: TodoRepository) {
 
+    @CrossOrigin
     @GetMapping
     fun getTodos() = todoRepository.findAll()
 
+    @CrossOrigin
     @GetMapping("/{todoId}")
     fun getTodo(@PathVariable("todoId") todoId: Long): Optional<Todo> {
         return todoRepository.findById(todoId)
     }
 
+    @CrossOrigin
     @PostMapping
     fun newTodo(@RequestBody todo: Todo): Todo{
         return todoRepository.save(todo)
     }
 
+    @CrossOrigin
     @PutMapping("/{todoId}")
     fun updateTodo(@PathVariable("todoId") todoId: Long, @RequestBody updatedTodo: Todo): Todo? {
         val oldTodo = todoRepository.findByIdOrNull(todoId)
@@ -34,6 +39,7 @@ class ToDoController(val todoRepository: TodoRepository) {
         return todoRepository.save(updatedTodo)
     }
 
+    @CrossOrigin
     @DeleteMapping("/{todoId}")
     fun deleteTodo(@PathVariable("todoId") todoId: Long){
         todoRepository.deleteById(todoId)
